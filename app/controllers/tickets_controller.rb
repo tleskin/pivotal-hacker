65064c2921c420e1e945fb9a75cfb86b46b6bf79
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-16 10:50:39 -0600

    added create route for tickets and updated create action in tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,20 @@ class TicketsController < ApplicationController
   def new
     @ticket = Ticket.new
   end
+
+  def create
+    ticket = Ticket.new(ticket_params)
+    if ticket.save
+      redirect_to board_path(ticket.board)
+    else
+      flash[:errors] = "You need a Title. #{ticket.errors.full_messages}"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
+  end
 end
