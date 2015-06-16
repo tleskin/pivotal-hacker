7e9ec96f4feb70e81b7ea90845dfccaa62d2401a
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-16 11:35:49 -0600

    updated custom callback

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,11 @@
 class Ticket < ActiveRecord::Base
   validates :title, :description, presence: true
 
-  after_initialize :default_password
+  belongs_to :board
 
-  def default_password
+  after_initialize :default_status
+
+  def default_status
     self.status ||= "Backlog" if self.new_record?
   end
 end
