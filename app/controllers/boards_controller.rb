708084b7aab264c9a13af201337fbe925944737d
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-16 10:00:50 -0600

    create action in board controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,20 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    board = Board.new(board_params)
+    if board.save
+      redirect_to board_path(board)
+    else
+      flash[:errors] = "You need a Title. #{board.errors.full_messages}"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
