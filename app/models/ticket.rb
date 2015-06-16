4e11ff27d4d27207ddd027779c116b50557a7fbe
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-16 10:47:58 -0600

    assigned default status through AR after_initialize callback

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,9 @@
 class Ticket < ActiveRecord::Base
+  validates :title, :description, presence: true
 
+  after_initialize :default_password
+
+  def default_password
+    self.status ||= "Backlog" if self.new_record?
+  end
 end
