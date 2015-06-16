f0e044ea99ff04a4d4d8f38b72f34857027c3c7f
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-16 09:50:09 -0600

    created spec for board creation

--- spec/features/user_creates_board_spec.rb
+++ spec/features/user_creates_board_spec.rb
@@ -7,4 +7,11 @@ RSpec.feature "a board" do
     click_link_or_button "Create a New Board"
     expect(page).to have_content("Please Enter A Title For Your Board")
   end
+
+  it "can be created by a user" do
+    visit new_board_path
+    fill_in "board[title]", with: "Pivotal-Slacker"
+    click_link_or_button("Create Board")
+    expect(page).to have_content("Your Board: Pivotal-Slacker")
+  end
 end
